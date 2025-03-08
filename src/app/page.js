79e7189f558a2b9dc7749d4a652a5ca22e8a03b7
Thomas Janko <thomas78125@gmail.com>
2025-03-08 11:43:09 +0100

    gradient bg

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import WalletConnect from "@/components/shared/WalletConnect";
 
 export default function Home() {
   return (
-    <div className="">
-      <WalletConnect />
+    <div className="min-h-screen gradient-bg-welcome">
+      <div className="p-4">
+        <WalletConnect />
+      </div>
     </div>
   );
 }
